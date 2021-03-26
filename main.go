594a9ff22d918c08ab7766a0bf389e6fd32c4551
Matt Welke <mattwelke@gmail.com>
2021-03-26 00:38:30 -0400

    with server

--- main.go
+++ main.go
@@ -2,14 +2,29 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"os"
 	"time"
 )
 
 func main() {
-	timeStart := time.Now().UTC()
-	for {
-		time.Sleep(time.Duration(1 * time.Second))
-		diffStart := time.Now().UTC().Sub(timeStart)
-		fmt.Printf("time since startup: %s\n", diffStart)
+	go func() {
+		timeStart := time.Now().UTC()
+		for {
+			time.Sleep(time.Duration(1 * time.Second))
+			diffStart := time.Now().UTC().Sub(timeStart)
+			fmt.Printf("time since startup: %s\n", diffStart)
+		}
+	}()
+
+	// Server just to keep platform happy
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
 	}
+	http.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
